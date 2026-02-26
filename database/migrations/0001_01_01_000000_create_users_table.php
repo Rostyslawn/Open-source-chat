04743c553596c3bbf86291ce4855130ab31a8149
Rostyslaw <75090630+Rostyslawn@users.noreply.github.com>
2026-02-26 20:46:34 +0200

    unique nicknames

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('avatar')->default('avatars/default.png');
             $table->string('password');
             $table->boolean('verified')->default(false);
